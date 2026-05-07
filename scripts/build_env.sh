6c54d7c85791dd08c6965670e38d744b6bf3795f
royalneverwin <2301213100@pku.edu.cn>
2026-05-07 19:26:12 +0800

    update

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -1,4 +1,4 @@
 uv venv --python 3.10
 source .venv/bin/activate
 
-uv pip install --torch-backend=auto -e '.[vllm,gui,rag,code_interpreter,mcp]'
+uv pip install --torch-backend=cu129 -e '.[vllm,gui,rag,code_interpreter,mcp]'
